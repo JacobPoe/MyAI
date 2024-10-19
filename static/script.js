a354828791716ea63780d83480b4442cb3b3098d
Jake Poe <jacobpoe@gmail.com>
2024-10-19 10:39:53 -0400

    Basic react gui - good enough for now

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
 import { createRoot } from "react-dom/client";
 
 const VoiceAssistant = () => {
@@ -7,112 +7,118 @@ const VoiceAssistant = () => {
   const [voiceOption, setVoiceOption] = useState("default");
   const [lightMode, setLightMode] = useState(true);
   const [messages, setMessages] = useState([]);
-  const [loadingUser, setLoadingUser] = useState(false);
   const [loadingBot, setLoadingBot] = useState(false);
   const recorderRef = useRef(null);
   const baseUrl = window.location.origin;
 
-  const toggleLightMode = () => setLightMode(!lightMode);
-
-  const cleanTextInput = (value) => {
-    return value
-      .trim()
-      .replace(/[\n\t]/g, "")
-      .replace(/<[^>]*>/g, "")
-      .replace(/[<>&;]/g, "");
-  };
-
-  const handleSendMessage = async () => {
-    if (message.trim()) {
-      setMessages([...messages, { type: "user", content: message }]);
-      setMessage(""); // Clear input
-      await populateBotResponse(message, "TTS");
-    }
-  };
-
-  const toggleRecording = async () => {
-    if (!recording) {
-      // Start recording
-      recorderRef.current = await recordAudio();
-      recorderRef.current.start();
-      setRecording(true);
-    } else {
-      // Stop recording and process audio
-      const audio = await recorderRef.current.stop();
-      const userMessage = await getSpeechToText(audio);
-      setMessages([...messages, { type: "user", content: userMessage }]);
-      await populateBotResponse(userMessage, "STT");
-      setRecording(false);
-    }
-  };
-
-  const getSpeechToText = async (userRecording) => {
+  const handleAudioInput = async (userRecording) => {
     const response = await fetch(`${baseUrl}/speech-to-text`, {
       method: "POST",
       body: userRecording.audioBlob,
       headers: { "Content-Type": "audio/mpeg" },
     });
     const data = await response.json();
-    return data.text;
+    setMessages([...messages, { type: "transcription", content: data.text }]);
   };
 
-  const populateBotResponse = async (userMessage, inputType) => {
+  const handleAudioPlayback = async (data) => {
+    const df = document.createDocumentFragment();
+    const blob = await data.blob();
+    const url = URL.createObjectURL(blob);
+    const audio = new Audio(url);
+
+    df.appendChild(audio); // keep in fragment until finished playing
+    audio.addEventListener("ended", function () {
+      df.removeChild(audio);
+    });
+    audio.play().catch(error => console.error('Error playing audio:', error));
+    return audio;
+  }
+
+  const handleTextInput = async () => {
+    setMessages([...messages, { type: "user", content: message }]);
     setLoadingBot(true);
-    const payload = inputType === "TTS" ? JSON.stringify({ userMessage, voice: voiceOption }) : userMessage;
-    const headers = inputType === "TTS" ? { "Content-Type": "application/json" } : { "Content-Type": "audio/mpeg"};
+
     const response = await fetch(`${baseUrl}/text-to-speech`, {
       method: "POST",
-      body: payload,
-      headers,
+      body: JSON.stringify({ userMessage: message, voice: voiceOption }),
+      headers: { "Content-Type": "application/json" },
     });
-    const data = await response.json();
-    setMessages((prevMessages) => [...prevMessages, { type: "bot", content: data.text }]);
+
+    await handleAudioPlayback(response);
     setLoadingBot(false);
+    setMessage(""); // Clear input
+  };
+
+  const sanitize = (value) => {
+    return value
+      .replace(/[\n\t]/g, "")
+      .replace(/<[^>]*>/g, "")
+      .replace(/[<>&;]/g, "");
+  };
+  const toggleLightMode = () => setLightMode(!lightMode);
+  const toggleRecording = async () => {
+    const isRecording = !recording;
+    setRecording(isRecording);
+
+    if (!recording) {
+      // Start recording
+      recorderRef.current = await recordAudio();
+      recorderRef.current.start();
+    } else {
+      // Stop recording and process audio
+      const audio = await recorderRef.current.stop();
+      await handleAudioInput(audio);
+    }
   };
 
   return (
     <>
       <div className={`myai-container ${lightMode ? "light-mode" : "dark-mode"}`}>
-      <div className="row">
-        <div className="col-12 text-center">
-          <h1>Voice Assistant</h1>
-          <p>Your personal assistant powered by Suno's TTS model.</p>
-          <div>
-            <label>
-              <input type="checkbox" checked={lightMode} onChange={toggleLightMode} /> Toggle light/dark mode
-            </label>
+        <div className="row">
+          <div className="col-12 text-center">
+            <h1>Voice Assistant</h1>
+            <p>Your personal assistant powered by Suno's TTS model on <a href="https://huggingface.co/suno/bark">HuggingFace</a>.</p>
+            <div>
+              <label>
+                <input type="checkbox" checked={lightMode} onChange={toggleLightMode} /> Toggle light/dark mode
+              </label>
+            </div>
           </div>
-        </div>
 
-        <div className="col-12 col-md-8 mx-auto">
-          <div id="chat-window">
-            {messages.map((msg, index) => (
-              <div key={index} className={`message-line ${msg.type}`}>
-                <div className={`message-box ${msg.type === "bot" ? "bot-text" : "my-text"}`}>
-                  {msg.content}
+          <div className="col-12 col-md-8 mx-auto">
+            <div id="chat-window">
+              {messages.map((msg, index) => (
+                <div key={index} className={`message-line ${msg.type}`}>
+                  <div className={`message-box ${msg.type === "bot" ? "bot-text" : ""}`}>
+                    {msg.content}
+                  </div>
                 </div>
-              </div>
-            ))}
-            {loadingBot && <div>Bot is typing...</div>}
-          </div>
+              ))}
+              {loadingBot && <div>Bot is typing...</div>}
+            </div>
 
-          <div className="input-group mt-1">
-            <input
-              type="text"
-              value={message}
-              onChange={(e) => setMessage(cleanTextInput(e.target.value))}
-              className="form-control"
-              placeholder="Type your message here..."
-            />
-            <div className="input-group-append">
+            <div className="input-group mt-1">
+              <input
+                  type="text"
+                  value={message}
+                  onChange={(e) => setMessage(sanitize(e.target.value))}
+                  className="form-control"
+                  placeholder="Type your message here..."
+              />
+              <div className="input-group-append">
+                <button onClick={handleTextInput} className="btn btn-primary">
+                  Send
+                </button>
+              </div>
+
               <button onClick={toggleRecording} className="btn btn-primary">
-                {recording ? <i className="fa fa-stop"></i> : <i className="fa fa-microphone"></i>}
+                {recording ? <i className="fa fa-stop">[STOP]</i> : <i className="fa fa-microphone">[START]</i>}
               </button>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
