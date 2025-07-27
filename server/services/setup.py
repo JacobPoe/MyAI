e345cfac032bd671233fed2c3b17624bab2f8b00
Jake Poe <jacobpoe@gmail.com>
2025-07-27 13:49:10 -0400

    Updates services package version no.

--- server/services/setup.py
+++ server/services/setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="services",  # Name of your module
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),  # Automatically find all packages
 )
