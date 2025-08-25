86dc24b688c9332116da467f91243b95dfe35c04
Jake Poe <jacobpoe@gmail.com>
2025-08-25 00:01:08 -0400

    Fix AudioService.load_audio()

--- server/services/audio.py
+++ server/services/audio.py
@@ -1,33 +1,42 @@
 import io
 import scipy.io.wavfile as wav
 
+from enum import Enum
 from pydub import AudioSegment
 
+from services.env import EnvService, EnvVars
 from utils.logger import Logger, LogLevel
 
+AUDIO_SAMPLE_RATE = EnvService.get_int(EnvVars.AUDIO_SAMPLE_RATE.value, 16000)
+DEBUG = EnvService.is_debug()
+
+
+class AudioFormat(Enum):
+    WAV = "wav"
+    WEBM = "webm"
 
 class AudioService:
     @staticmethod
-    def load_audio(data, debug=False):
+    def load_audio(raw_bytes):
         Logger.log(LogLevel.INFO, "Loading audio data from request...")
 
-        if not data or len(data) == 0:
+        if not raw_bytes or len(raw_bytes) == 0:
             Logger.log(LogLevel.ERROR, "Received empty audio data.")
             return None
 
-        audio_buffer = io.BytesIO(data)
+        audio_buffer = io.BytesIO(raw_bytes)
         wav_buffer = io.BytesIO()
 
-        audio = AudioSegment.from_file(audio_buffer)
-        audio.export(wav_buffer, format="wav")
+        audio_segment = AudioSegment.from_file(audio_buffer, format=AudioFormat.WEBM.value)
+        audio_segment = audio_segment.set_frame_rate(AUDIO_SAMPLE_RATE).set_channels(1)
+        audio_segment.export(wav_buffer, format=AudioFormat.WAV.value)
         wav_buffer.seek(0)
 
-        if debug:
-            Logger.log(LogLevel.DEBUG, "Saving audio data to file `debug.raw`.")
-            with open("debug.raw", "wb") as f:
-                f.write(audio_buffer.read())
+        if DEBUG:
+            Logger.log(LogLevel.DEBUG, "Saving audio prompt to debug.wav")
+            with open("debug.wav", "wb") as f:
+                f.write(wav_buffer.getvalue())
 
-        # Read the audio data from the wav_buffer
         sampling_rate, audio_data = wav.read(wav_buffer)
         Logger.log(LogLevel.INFO, "Audio data loaded successfully.")
 
